70efeaea707cbb136ec16c9bbe7f04b573a842cc
hutao <mm_Huang_work@163.com>
2024-02-27 23:34:57 +0800

    feat: 引入tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>About</h1>
+        <h1 class="flex">About</h1>
     </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,28 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import AutoImportComponents from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      // dirs: ['src/autoImports'],
+      imports: ['vue', 'vue-router'],
+      eslintrc: {
+        enabled: true
+      },
+      dts: 'src/auto-imports.d.ts'
+    }),
+    AutoImportComponents({
+      dirs: ['src/fyComponents', 'src/components'],
+      extensions: ['vue'],
+      deep: true,
+      dts: 'src/components.d.ts'
+    })
   ],
   resolve: {
     alias: {
