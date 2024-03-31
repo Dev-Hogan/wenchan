d195fed9477923f95651be861046611b7cb0aaa3
Dev Hogan <mm_Huang_work@163.com>
2024-03-31 15:40:00 +0800

    feat: 顶部操作

--- src/modules/Tag.vue
+++ src/modules/Tag.vue
@@ -2,11 +2,11 @@
     <NtHeader title="标签">
         <template #edit>
             <div class="flex items-center space-x-3 [&>button]:border [&>button]:border-light-2 [&>button]:rounded-4">
-                <NtButton>前往</NtButton>
+                <NtButton @click="handelGo2tag">前往</NtButton>
                 <NtButton @click="handleMerge">合并</NtButton>
-                <NtButton>重命名</NtButton>
+                <NtButton @click="handleRename">重命名</NtButton>
                 <NtButton>删除</NtButton>
-                <NtButton class="h-[36px]">
+                <NtButton @click="handleOrder" class="h-[36px]">
                     <NtIcon icon="sort2"></NtIcon>
                 </NtButton>
             </div>
@@ -14,7 +14,8 @@
     </NtHeader>
     <NtContent>
         <div class="flex flex-wrap gap-5 mt-[33px]">
-            <NtButton :class="['border rounded-3 bg-light space-x-2', item.isSelected ? '[&>span]:text-white bg-theme' : '']"
+            <NtButton
+                :class="['border rounded-3 bg-light space-x-2', item.isSelected ? '[&>span]:text-white bg-theme' : '']"
                 v-for="item in tags" :key="item.id" @click="select(item)">
                 <span class="text-light-6">{{
                     item.name }}</span><span class="text-light-5 text-[12px]">{{
@@ -22,11 +23,14 @@
             </NtButton>
         </div>
     </NtContent>
+    <!-- 新增分类弹窗 -->
+    <EditCategoryDialog v-model:open="categoryOpen" v-model:id="categoryId"></EditCategoryDialog>
 </template>
 
 <script setup lang="ts">
 import { message } from 'ant-design-vue';
-
+const categoryOpen = ref(false)
+const categoryId = ref<number>()
 const arr = new Array(35).fill(1)
 type Tag = {
     name: string
@@ -84,4 +88,24 @@ function handleMerge() {
     cannotMerge()
 
 }
+function handleRename() {
+    if (selectKeys.value.length >= 2 || selectKeys.value.length === 0) {
+        message.warn('无法重命名')
+        return
+    }
+    categoryOpen.value = true
+    categoryId.value = selectKeys.value[0]
+}
+
+function handelGo2tag() {
+    if (selectKeys.value.length >= 2 || selectKeys.value.length === 0) {
+        message.warn('无法前往复数标签')
+        return
+    }
+}
+
+function handleOrder() {
+    tags.value.sort((a, b) => b.count - a.count)
+}
+
 </script>
