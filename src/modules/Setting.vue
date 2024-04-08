f91ed51b670c0c7e22ee46524d7ef58b8749aa55
Dev Hogan <mm_Huang_work@163.com>
2024-04-08 23:43:02 +0800

    feat: 设置页面

--- src/modules/Setting.vue
+++ src/modules/Setting.vue
@@ -29,14 +29,72 @@
 
             </div>
             <NtButton
-                class="items-center min-h-0 border border-theme text-theme font-bold text-[14px] bg-theme-15 rounded-5 !px-8 py-5 h-[37px]">
+                class="leading-4 items-center min-h-0 border border-theme text-theme font-bold text-[14px] bg-theme-15 rounded-5 !px-8 py-5 h-[37px]">
                 GO
                 PREMIUM</NtButton>
         </div>
+        <div>
+            <div class="font-medium">邮箱</div>
+            <NtInput v-model="email" class="mt-[5px] mb-[11px] h-[46px] text-light-7 text-[14px]"></NtInput>
+            <NtButton class="_button">修改邮箱</NtButton>
+        </div>
+        <div>
+            <div class="font-medium">外观</div>
+            <div class="flex space-x-5 text-center mt-8">
+                <div v-for="item in themes" :key="item.id" class="space-y-5 cursor-pointer">
+                    <img :src="item.img">
+                    <div class="text-[13px] text-light-5">{{ item.name }}</div>
+                </div>
+            </div>
+        </div>
+        <div class="space-y-8">
+            <div class="flex space-x-2">
+                <div class="font-medium">自动同步</div>
+                <div class="inline-block bg-theme rounded-[3px] px-2 py-[1px] text-default">PRO</div>
+            </div>
+            <div class="space-x-7">
+                <NtButton class="_button">备份数据</NtButton>
+                <NtButton class="_button">导入备份</NtButton>
+            </div>
+        </div>
+        <div class="space-y-8">
+            <div class="font-medium">危险操作</div>
+            <NtButton class="_button !text-delete">导入备份</NtButton>
+        </div>
+
+        <div class="absolute bg-default z-30 bottom-[26px] left-[50%] translate-x-[-50%] space-x-10 text-light-4"><span
+                class="cursor-pointer">用户协议</span><span class="text-light-2">|</span><span
+                class="cursor-pointer">隐私政策</span></div>
     </NtContent>
 </template>
 
 <script setup lang="ts">
 import avatar from '@/assets/avatar.png';
-
+import themeLight from "@/assets/theme-light.png";
+import themeDark from '@/assets/theme-dark.png'
+import themeSunny from "@/assets/theme-sunny.png";
+import themeSea from "@/assets/theme-sea.png";
+const email = ref<string>('Wenchan@gmail.com')
+const themes = [{
+    id: 1,
+    name: '浅色',
+    img: themeLight
+}, {
+    id: 2,
+    name: '暗色',
+    img: themeDark
+}, {
+    id: 3,
+    name: 'Sunny',
+    img: themeSunny
+}, {
+    id: 4,
+    name: 'Sea',
+    img: themeSea
+}]
 </script>
+<style scoped>
+._button {
+    @apply border border-light-2 rounded-4 px-8 py-5 text-light-7 font-medium;
+}
+</style>
